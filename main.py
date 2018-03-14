c7dd88b833aadd24aa83966c98c9600708d4ee27
yoshietao <yoshietao@g.ucla.edu>
2018-03-14 15:25:15 -0700

    sys.argv

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from SIFT1 import SIFT_
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
+import sys
 
 
 def match_point(f1,f2):
-	print('Matching points...')
 	f_comb = []
 	for f in f1:				#f = feature
 		max_cos = 0
@@ -21,8 +21,7 @@ def match_point(f1,f2):
 		f_comb.append([i for i in f]+[x-f[0],y-f[1],max_cos])
 	return f_comb
 
-def RANSAC(col,f1,f2,th=5):
-	print('RANSAC...')
+def RANSAC(im1,im2,f1,f2,th=5):
 	max_xy = [0,0]
 	max_count = 0
 	f1m = []
@@ -46,22 +45,25 @@ def RANSAC(col,f1,f2,th=5):
 			max_xy = [f[-3],f[-2]]
 			f1m = f1_
 			f2m = f2_
-	
-	im1 = Image.open('purdue/im1.jpg')
-	im2 = Image.open('purdue/im2.jpg')
-	#visualization(im1,im2,f1m,f2m)
+
+	visualization(im1,im2,f1m,f2m)
 	print(max_count)
 	return -max_xy[0],-max_xy[1]
 
-def registration(col,f1,f2):
+def registration(im1,im2,f1,f2):
 	print('registration processing...')
+	print('Matching points...')
 	if not Path('f1_.npy').exists():
 		f1 = match_point(f1,f2)
 		np.save('f1_.npy',f1)
 	f1 = np.load('f1_.npy')
-	x,y = RANSAC(col,f1,f2)
-	print(x,y)
-	return int(x),int(y)
+	print('RANSAC...')
+	if not Path('match.npy').exists():
+		x,y = RANSAC(im1,im2,f1,f2)
+		np.save('match.npy',[x,y])
+	xy = np.load('match.npy')
+	print(xy)
+	return int(xy[0]),int(xy[1])
 
 def visualization(im1,im2,f1,f2):
 	plt.figure()
@@ -80,14 +82,13 @@ def test():
 	x2 = x1
 	registration(np.zeros((3,3)),x1,x2)
 
-def main():
-	img01 = Image.open('purdue/im1.jpg')
-	print(img01)
+def main(im1='purdue/im1.jpg', im2='purdue/im2.jpg'):
+	img01 = Image.open(im1)
 	img1 = img01.convert('L')
-	img02 = Image.open('purdue/im2.jpg')
+	img02 = Image.open(im2)
 	img2 = img02.convert('L')
 	
-	print(np.array(img1).shape)
+	print('Shape of image1:',np.array(img1).shape)
 
 	if not Path('f1.npy').exists():
 		s1 = SIFT_(np.array(img1))
@@ -101,9 +102,8 @@ def main():
 	f1 = np.load('f1.npy')
 	f2 = np.load('f2.npy')
 	#print([i[:2] for i in f1])
-	print(np.array(img01).shape)
 	row,col,_ = np.array(img01).shape
-	x,y = registration(col,f1,f2)
+	x,y = registration(img01,img02,f1,f2)
 
 	if x >= 0:
 		new_img = np.zeros([row+x,col+y,3])
@@ -115,19 +115,16 @@ def main():
 		new_img[-row:,:col,:] = img01
 		new_img[:row,-col:,:] += img02
 		new_img[-x:row,y:col,:] /=2
-	if 0:
-		new_img = np.zeros([row,col+y,3])
-		new_img[:,:col,:] = img01
-		new_img[:,-col:,:] += img02
-		new_img[:,y:col,:] /=2
 	
 	im = Image.fromarray(new_img.astype('uint8'))
 	im.show()
 	im.save('result.jpg')
 
 if __name__ == '__main__':
-	main()
-	#test()
+	if len(sys.argv) == 2:
+		main(sys.argv[1],sys.argv[2])
+	else:
+		main()
 
 
 
